7faf012348fae0b31f38cb6a0377456d0f10b9e2
mongtho <mongthodang07032002@gmail.com>
2023-09-18 19:33:53 +0700

    DangThiMongTho_Tuan2

--- main.js
+++ main.js
@@ -43,13 +43,7 @@ console.log(" Data 1 - Mark has a higher BMI :", markHigherBMI1);
 console.log(" Data 1 - Mark has a higher BMI :", markHigherBMI2);
 
 // Coding Challenge #2
-// Use the BMI example from Challenge #1, and the code you already wrote, and
-// improve it.
-// Your tasks:
-// 1. Print a nice output to the console, saying who has the higher BMI. The message
-// is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
-// 2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
-// BMI (28.3) is higher than John's (23.9)!"
+
 
 if (markBMI1 > JohnBMI1) {
     console.log(" data 1 - mark's BMI (${markBMI1.toFixed(1)}) is higher than jons's (${JohnBMI1.toFixed(1)})!");
@@ -66,25 +60,7 @@ if (markBMI2 > JohnBMI2) {
     console.log("Data 2 - Mark and john have the same BMI");
 }
 // Coding Challenge #3
-// There are two gymnastics teams, Dolphins and Koalas. They compete against each
-// other 3 times. The winner with the highest average score wins a trophy!
-// Your tasks:
-// 1. Calculate the average score for each team, using the test data below
-// 2. Compare the team's average scores to determine the winner of the competition,
-// and print it to the console. Don't forget that there can be a draw, so test for that
-// as well (draw means they have the same average score)
-// 3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
-// team only wins if it has a higher score than the other team, and the same time a
-// score of at least 100 points. Hint: Use a logical operator to test for minimum
-// score, as well as multiple else-if blocks 
-// 4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
-// both teams have the same score and both have a score greater or equal 100
-// points. Otherwise, no team wins the trophy
-// Test data:
-// § Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
-// § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
-// § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
-// Function to calculate the average score
+
 function calculateAverage(scores) {
     const sum = scores.reduce((acc, score) => acc + score, 0);
     return sum / scores.length;
@@ -124,23 +100,7 @@ function calculateAverage(scores) {
   const minimumScore3 = 100;
   determineWinner(dolphinsScores3, koalasScores3, minimumScore3);
 //   Coding Challenge #4
-// Steven wants to build a very simple tip calculator for whenever he goes eating in a
-// restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
-// 300. If the value is different, the tip is 20%.
-// Your tasks:
-// 1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
-// this. It's not allowed to use an if/else statement  (If it's easier for you, you can
-// start with an if/else statement, and then try to convert it to a ternary
-// operator!)
-// 2. Print a string to the console containing the bill value, the tip, and the final value
-// (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
-// 316.25”
-// Test data:
-// § Data 1: Test for bill values 275, 40 and 430
-// Hints:
-// § To calculate 20% of a value, simply multiply it by 20/100 = 0.2
-// § Value X is between 50 and 300, if it's >= 50 && <= 300
-// Test data
+
 const billValues = [275, 40, 430];
 
 // Calculate the tip using a ternary operator
@@ -156,26 +116,7 @@ totalValues.forEach((total, index) => {
   console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${total}`);
 });
 // Coding Challenge #5
-// Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
-// gymnastics discipline, which works differently.
-// Each team competes 3 times, and then the average of the 3 scores is calculated (so
-// one average score per team).
-// A team only wins if it has at least double the average score of the other team.
-// Otherwise, no team wins!
-// Your tasks:
-// 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
-// 2. Use the function to calculate the average for both teams
-// 3. Create a function 'checkWinner' that takes the average score of each team
-// as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
-// to the console, together with the victory points, according to the rule above.
-// Example: "Koalas win (30 vs. 13)"
-// 4. Use the 'checkWinner' function to determine the winner for both Data 1 and
-// Data 2
-// 5. Ignore draws this time
-// Test data:
-// § Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
-// § Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
-// Create an arrow function to calculate the average of 3 scores
+
 const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
 // Create a function to check the winner and log the result
@@ -248,6 +189,360 @@ const mark = {
   } else {
     console.log(`${mark.fullName} and ${john.fullName} have the same BMI (${mark.bmi.toFixed(1)})!`);
   }
-
-
+  // A Closer Look at Functions
+  // Coding Challenge #1
+  const poll = {
+    question: 'What is your favorite programming language?',
+    options: ['JavaScript', 'Python', 'Rust', 'C++'],
+    answers: new Array(4).fill(0), // Initialize answers array with zeros
   
+    registerNewAnswer() {
+      const userInput = prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      );
+  
+      const answer = parseInt(userInput);
+  
+      if (!isNaN(answer) && answer >= 0 && answer < this.options.length) {
+        this.answers[answer]++;
+      } else {
+        alert('Invalid input. Please enter a valid option number.');
+      }
+  
+      this.displayResults();
+    },
+  
+    displayResults(type = 'array') {
+      if (type === 'string') {
+        console.log(`Poll results are ${this.answers.join(', ')}`);
+      } else {
+        console.log(this.answers);
+      }
+    },
+  };
+  
+  // Test the poll app with bonus data
+  const bonusData1 = [5, 2, 3];
+  const bonusData2 = [1, 5, 3, 9, 6, 1];
+  
+  poll.displayResults('array'); // Display initial results as an array
+  poll.displayResults('string'); // Display initial results as a string
+  
+  // Register new answers for bonus data
+  poll.answers = bonusData1;
+  poll.registerNewAnswer();
+  poll.answers = bonusData2;
+  poll.registerNewAnswer();
+
+  // Coding Challenge #2
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.body.addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+
+// Working With Arrays
+// Coding Challenge #1
+const checkDogs = function (dogsJulia, dogsKate) {
+  // Create a shallow copy of Julia's data and remove cat ages
+  const correctedJulia = [...dogsJulia];
+  correctedJulia.splice(0, 1); // Remove the age of the first cat
+  correctedJulia.splice(-2);   // Remove the ages of the last two cats
+
+  // Create an array with both Julia's corrected data and Kate's data
+  const allDogs = [...correctedJulia, ...dogsKate];
+
+  // Log whether each dog is an adult or a puppy
+  allDogs.forEach(function (age, i) {
+    const dogType = age >= 3 ? 'adult' : 'puppy';
+    console.log(`Dog number ${i + 1} is an ${dogType}, and is ${age} years old.`);
+  });
+};
+
+// Test data
+const data1Julia = [3, 5, 2, 12, 7];
+const data1Kate = [4, 1, 15, 8, 3];
+
+const data2Julia = [9, 16, 6, 8, 3];
+const data2Kate = [10, 5, 6, 1, 4];
+
+// Run the function for both test datasets
+console.log('--- Data 1 ---');
+checkDogs(data1Julia, data1Kate);
+
+console.log('--- Data 2 ---');
+checkDogs(data2Julia, data2Kate);
+
+// Coding Challenge #2
+const calcAverageHumanAge = function (ages) {
+  // Calculate dog age in human years and filter dogs >= 18 human years old
+  const humanAges = ages
+    .map(dogAge => (dogAge <= 2 ? 2 * dogAge : 16 + dogAge * 4))
+    .filter(humanAge => humanAge >= 18);
+
+  // Calculate the average human age of all adult dogs
+  const sumHumanAges = humanAges.reduce((acc, age) => acc + age, 0);
+  const averageHumanAge = sumHumanAges / humanAges.length;
+
+  return averageHumanAge;
+};
+
+// Test data
+const data1 = [5, 2, 4, 1, 15, 8, 3];
+const data2 = [16, 6, 10, 5, 6, 1, 4];
+
+// Calculate and display the average human age for both datasets
+const averageAge1 = calcAverageHumanAge(data1);
+const averageAge2 = calcAverageHumanAge(data2);
+
+console.log('Average Human Age for Data 1: ${averageAge1.toFixed(2)} years');
+console.log('Average Human Age for Data 2: ${averageAge2.toFixed(2)} years');
+
+// Coding Challenge #3
+// Coding Challenge #4
+// Test data
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Bob', 'Alice'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+// Step 1: Calculate recommended food portion and add it to each dog object
+dogs.forEach(dog => {
+  dog.recommendedFood = Math.trunc(dog.weight ** 0.75 * 28);
+});
+
+// Step 2: Find Sarah's dog and log whether it's eating too much or too little
+const sarahDog = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(
+  `Sarah's dog is eating ${
+    sarahDog.curFood > sarahDog.recommendedFood ? 'too much' : 'too little'
+  }`
+);
+
+// Step 3: Create arrays for owners of dogs who eat too much or too little
+const ownersEatTooMuch = dogs
+  .filter(dog => dog.curFood > dog.recommendedFood)
+  .flatMap(dog => dog.owners);
+const ownersEatTooLittle = dogs
+  .filter(dog => dog.curFood < dog.recommendedFood)
+  .flatMap(dog => dog.owners);
+
+// Step 4: Log strings for owners of dogs who eat too much or too little
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
+
+// Step 5: Check if any dog is eating exactly the recommended amount
+const anyDogEatingRecommendedAmount = dogs.some(
+  dog => dog.curFood === dog.recommendedFood
+);
+console.log(
+  `Any dog eating exactly the recommended amount: ${anyDogEatingRecommendedAmount}`
+);
+
+// Step 6: Check if any dog is eating an okay amount (between 90% and 110%)
+const anyDogEatingOkayAmount = dogs.some(
+  dog =>
+    dog.curFood >= dog.recommendedFood * 0.9 &&
+    dog.curFood <= dog.recommendedFood * 1.1
+);
+console.log(`Any dog eating an okay amount: ${anyDogEatingOkayAmount}`);
+
+// Step 7: Create an array of dogs eating an okay amount
+const dogsEatingOkayAmount = dogs.filter(
+  dog =>
+    dog.curFood >= dog.recommendedFood * 0.9 &&
+    dog.curFood <= dog.recommendedFood * 1.1
+);
+
+// Step 8: Create a shallow copy of 'dogs' and sort it by recommended food portion
+const dogsSortedByRecommendedFood = [...dogs].sort(
+  (a, b) => a.recommendedFood - b.recommendedFood
+);
+
+console.log(dogsSortedByRecommendedFood);
+
+// Object Oriented Programming (OOP)
+// Coding Challenge #1
+// 1. Create a 'Car' constructor function
+function Car(make, speed) {
+  this.make = make;
+  this.speed = speed;
+}
+
+// 2. Implement the 'accelerate' method
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is accelerating. New speed: ${this.speed} km/h`);
+};
+
+// 3. Implement the 'brake' method
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is braking. New speed: ${this.speed} km/h`);
+};
+
+// 4. Create 2 'Car' objects and experiment with 'accelerate' and 'brake'
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+
+console.log(`Car 1: ${car1.make} is going at ${car1.speed} km/h`);
+console.log(`Car 2: ${car2.make} is going at ${car2.speed} km/h`);
+
+car1.accelerate(); // Accelerate car1
+car1.accelerate(); // Accelerate car1 again
+car1.brake();      // Brake car1
+
+car2.accelerate(); // Accelerate car2
+car2.brake();      // Brake car2
+
+// Coding Challenge #2
+// Coding Challenge #3
+
+// Parent class Car
+function Car(make, speed) {
+  this.make = make;
+  this.speed = speed;
+}
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is accelerating. New speed: ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is braking. New speed: ${this.speed} km/h`);
+};
+
+// Child class EV (Electric Car)
+function EV(make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+}
+
+// Inherit methods from Car
+EV.prototype = Object.create(Car.prototype);
+
+// Implement the chargeBattery method for EV
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+  console.log(`${this.make} battery charged to ${this.charge}%`);
+};
+
+// Override the accelerate method for EV
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge -= 1;
+  console.log(`${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`);
+};
+
+// Create an electric car object
+const evCar = new EV('Tesla', 120, 23);
+
+console.log(`Car 1: ${evCar.make} going at ${evCar.speed} km/h, with a charge of ${evCar.charge}%`);
+
+// Experiment with calling methods
+evCar.accelerate(); // Accelerate EV
+evCar.brake();      // Brake EV
+evCar.chargeBattery(90); // Charge the battery to 90%
+// Coding Challenge #4
+// ASYNCHRONOUS JAVASCRIPT
+// Coding Challenge #1
+// PART 1:
+
+async function whereAmI(lat, lng) {
+  try {
+    const response = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    if (!response.ok) throw new Error('Problem getting location data.');
+
+    const data = await response.json();
+    console.log(`You are in ${data.city}, ${data.country}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+}
+
+// Test with coordinates
+whereAmI(52.508, 13.381); // Berlin, Germany
+whereAmI(19.037, 72.873); // Mumbai, India
+whereAmI(-33.933, 18.474); // Cape Town, South Africa
+
+
+// PART 2:
+
+async function whereAmI(lat, lng) {
+  try {
+    const response = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    if (!response.ok) throw new Error('Problem getting location data.');
+
+    const data = await response.json();
+    console.log(`You are in ${data.city}, ${data.country}`);
+
+    const country = data.country;
+    const countryResponse = await fetch(`https://restcountries.com/v3/name/${country}`);
+    if (!countryResponse.ok) throw new Error('Problem getting country data.');
+
+    const countryData = await countryResponse.json();
+    console.log(`Country: ${countryData[0].name.common}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+}
+
+// Test with coordinates
+whereAmI(52.508, 13.381); // Berlin, Germany
+whereAmI(19.037, 72.873); // Mumbai, India
+whereAmI(-33.933, 18.474); // Cape Town, South Africa
+
+//
+function createImage(imgPath) {
+  return new Promise((resolve, reject) => {
+    const img = document.createElement('img');
+    img.src = imgPath;
+    img.addEventListener('load', () => {
+      document.querySelector('.images').appendChild(img);
+      resolve(img);
+    });
+    img.addEventListener('error', () => reject(new Error(`Image not found: ${imgPath}`)));
+  });
+}
+
+async function loadNPause(imgPath) {
+  try {
+    const img = await createImage(imgPath);
+    await wait(2000);
+    img.style.display = 'none';
+
+    const nextImgPath = 'img/img-2.jpg'; // Replace with the next image path
+    const nextImg = await createImage(nextImgPath);
+    await wait(2000);
+    nextImg.style.display = 'none';
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+// Test with image paths
+loadNPause('img/img-1.jpg');
+
+async function loadAll(imgArr) {
+  try {
+    const promises = imgArr.map(imgPath => createImage(imgPath));
+    const imgs = await Promise.all(promises);
+    imgs.forEach(img => img.classList.add('parallel'));
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+// Test with an array of image paths
+const imgArr = ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg'];
+loadAll(imgArr);
+
+
+
+
